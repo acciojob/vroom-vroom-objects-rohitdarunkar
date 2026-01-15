fdaaac1d2279492099a85c6ab3e3dba730be5891
Rohit Darunkar <139346362+rohitdarunkar@users.noreply.github.com>
2026-01-16 02:44:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+// Method on Car prototype
+Car.prototype.getMakeModel = function () {
+  return `${this.make} ${this.model}`;
+};
+
+function SportsCar(make, model, topSpeed) {
+  // inherit properties
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
+}
+
+// inherit prototype
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+// Method on SportsCar prototype
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
+ 
